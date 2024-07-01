866b4b1fe705062374e4d522dac384ad8cb0764d
Maduuz <dudinhathecat24@gmail.com>
2024-07-01 18:51:27 -0300

    aula com if simples 1.0.0
    Aprendemos como comentar em linha simples, em bloco, o que é variável, como ela atribui dados e um print que exibe no formato string ou texto

---
+++ python_if.py
@@ -0,0 +1,8 @@
+valor = 10 # A variável valor recebeu o número 10
+"""
+valor é a variável
+= atribui um dado à variável valor 10
+é um dado numérico
+"""
+if valor > 5: # O chato do Pedro disse que if é uma condição SE do Excel
+    print('O valor é maior que 5.')
