45a8721b7c697e4dcc0a45a11fee0cac8b1c2e3c
Maduuz <dudinhathecat24@gmail.com>
2024-07-01 19:09:15 -0300

    Trabalhando com condicional IF simples 1.1.0
    Aprendemos como comentar em linha simples, em bloco, o que é variável, como ela atribui dados e um print que exibe no formato string ou texto

--- python_if.py
+++ python_if.py
@@ -1,3 +1,6 @@
+"""
+Este programa é uma estrutura IF simples
+"""
 valor = 10 # A variável valor recebeu o número 10
 """
 valor é a variável
@@ -5,4 +8,15 @@ valor é a variável
 é um dado numérico
 """
 if valor > 5: # O chato do Pedro disse que if é uma condição SE do Excel
-    print('O valor é maior que 5.')
+    
+ """
+    if é o mesmo que CONDIÇÃO (SE)
+    valor é a variável que recebeu um dado numérico
+    >5 se o dado for maior que 5
+ """
+print('O valor é maior que 5.') # Exibe, imprime a informação na tela
+
+"""
+ A informação exibida no print pode ser entre aspas ou apostrofos 
+ pq dado é tipo texto, ou string
+"""
